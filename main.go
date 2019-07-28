0f54cc66954fddb15491db4b602cff4068706284
Borris Kasten <borris@amarantin.de>
2019-07-28 20:31:43 +0200

    Add first versio

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	fmt.Println("vim-go for github ci test 001")
+	fmt.Println("vim-go for github ci test v0.1.0")
 	for _, e := range os.Environ() {
 		pair := strings.Split(e, "=")
 		fmt.Println(pair[0], pair[1])
