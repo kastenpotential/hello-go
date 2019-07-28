83875b90dbf4bfce641db9175eed12d03455fafb
Borris Kasten <borris@amarantin.de>
2019-07-28 18:24:35 +0200

    Show env vars

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
 	fmt.Println("vim-go for github ci test 001")
+	for _, e := range os.Environ() {
+		pair := strings.Split(e, "=")
+		fmt.Println(pair[0], pair[1])
+	}
 }
