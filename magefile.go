b397d8ea6183b4a00deaf9150709d0a90c632536
Borris Kasten <borris@amarantin.de>
2019-07-29 11:02:55 +0200

    Add magefile test

---
+++ magefile.go
@@ -0,0 +1,24 @@
+//+build mage
+
+package main
+
+import (
+	"fmt"
+	"github.com/magefile/mage/sh"
+	"os"
+)
+
+// Runs dep ensure and then installs the binary.
+func Build() error {
+	if err := sh.Run("go", "vet", "./..."); err != nil {
+		return err
+	}
+	return sh.Run("go", "build", "-o", "hello-go")
+}
+
+func Deploy() error {
+	for key, value := range os.Environ() {
+		fmt.Printf("deploy: %s => %s\n", key, value)
+	}
+	return nil
+}
